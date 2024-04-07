9d8d66fd35b9c65540954d533b3c84525eb9e476
Brandon Graham <brandon@bellwood.io>
2024-04-07 12:35:02 -0400

    set base request mapping for test controller

--- src/main/java/com/bellwood/bellwoodinterview/controllers/Controller.java
+++ src/main/java/com/bellwood/bellwoodinterview/controllers/Controller.java
@@ -4,11 +4,13 @@ import com.bellwood.bellwoodinterview.models.Type;
 import com.bellwood.bellwoodinterview.services.ServiceA;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class Controller {
 
     @GetMapping("/{type}")
